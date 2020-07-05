11c3ac9551d2c62b31c753c4d8825fee263eb25e
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2020-07-05 15:35:34 -0400

    Enhance compare utility (#234)
    * Enhance compare utility
    
    * Fix GitHub Actions script

--- src/compare.py
+++ src/compare.py
@@ -15,17 +15,18 @@
 # limitations under the License.
 
 
+import sys
 import json
 from pathlib import Path
 from argparse import ArgumentParser
 from tempfile import TemporaryDirectory
-from typing import Set, List
+from typing import Any, Dict, List, Set
 
-from pandas import DataFrame
-from lib.io import read_file
+from pandas import DataFrame, concat
+from lib.io import read_file, display_progress
 from lib.net import download
+from lib.pipeline import DataPipeline
 from lib.utils import URL_OUTPUTS_PROD
-from update import main as update_data
 
 
 def compare_sets(curr: Set[str], prod: Set[str]) -> List[str]:
@@ -40,78 +41,87 @@ def compare_sets(curr: Set[str], prod: Set[str]) -> List[str]:
         List[str]: List of differences
     """
     diff_list: List[str] = []
-    for val in sorted(curr - prod):
-        diff_list += [f"+{val}"]
     for val in sorted(prod - curr):
         diff_list += [f"-{val}"]
+    for val in sorted(curr - prod):
+        diff_list += [f"+{val}"]
     return diff_list
 
 
-# pylint: disable=redefined-outer-name
-def main(output_folder: Path, only: List[str] = None, exclude: List[str] = None):
+def compare_tables(table_curr: DataFrame, table_prod: DataFrame) -> Dict[str, Any]:
+    cmp_out: Dict[str, Any] = {}
 
-    # Perform a dry-run to update the data using the current configuration
-    update_data(output_folder, only=only, exclude=exclude)
+    # Make a copy of the tables to avoid modification
+    table_curr = table_curr.copy()
+    table_prod = table_prod.copy()
 
-    # Download all the tables from the prod server to local storage
-    output_tables = list((output_folder / "tables").glob("*.csv"))
-    tables_summary = {table_path.stem: {} for table_path in output_tables}
-    for table_path in output_tables:
-        table_name = table_path.stem
-        table_path_str = str(table_path)
-        tables_summary[table_name]["local_curr"] = table_path_str
-        local_prod = Path(table_path_str.replace(".csv", ".prod.csv"))
-        with local_prod.open(mode="wb") as fd:
-            try:
-                download(f"{URL_OUTPUTS_PROD}/{table_path.name}", fd)
-                tables_summary[table_name]["local_prod"] = str(local_prod)
-            except:
-                tables_summary[table_name]["local_prod"] = None
+    # Compare the number of records
+    cmp_out["records"] = f"{len(table_curr) - len(table_prod):+d}"
 
-    # Compare the new vs prod data
-    for table_name, table_data in tables_summary.items():
+    # Compare the columns
+    cmp_out["columns"] = compare_sets(set(table_curr.columns), set(table_prod.columns))
 
-        # Read both tables into memory
-        curr_df = read_file(table_data["local_curr"])
-        if table_data["local_prod"] is None:
-            prod_df = DataFrame(columns=curr_df.columns)
-        else:
-            prod_df = read_file(table_data["local_prod"])
+    # Create a single, indexable column
+    idx_cols = ["key"] + (["date"] if "date" in table_curr.columns else [])
+    curr_idx, prod_idx = table_curr[idx_cols[0]], table_prod[idx_cols[0]]
+    for col in idx_cols[1:]:
+        curr_idx = curr_idx + " " + table_curr[col]
+        prod_idx = prod_idx + " " + table_prod[col]
 
-        # Compare the number of records
-        table_data["records"] = f"{len(curr_df) - len(prod_df):+d}"
+    # Compare the sets of indices
+    cmp_out["indices"] = compare_sets(set(curr_idx), set(prod_idx))
 
-        # Compare the columns
-        table_data["columns"] = compare_sets(set(curr_df.columns), set(prod_df.columns))
+    # Compare the shared indices
+    table_curr["_cmp_idx"] = curr_idx
+    table_prod["_cmp_idx"] = prod_idx
+    shared_df = concat([table_curr, table_prod]).drop_duplicates(keep=False)
+    cmp_out["modifications"] = shared_df["_cmp_idx"].values.tolist()
 
-        # Compare the keys
-        if "key" in curr_df.columns:
-            table_data["keys"] = compare_sets(set(curr_df.key.unique()), set(prod_df.key.unique()))
-
-        # Compare the dates
-        if "date" in curr_df.columns:
-            table_data["dates"] = compare_sets(
-                set(curr_df.date.unique()), set(prod_df.date.unique())
-            )
-
-    # Return the summary of changes
-    return tables_summary
+    return cmp_out
 
 
 if __name__ == "__main__":
 
     # Process command-line arguments
     argparser = ArgumentParser()
-    argparser.add_argument("output", type=str)
-    argparser.add_argument("--only", type=str, default=None)
-    argparser.add_argument("--exclude", type=str, default=None)
+    argparser.add_argument("--path", type=str, default=None)
+    argparser.add_argument("--table", type=str, default=None)
+    argparser.add_argument("--output", type=str, default=None)
+    argparser.add_argument("--progress", action="store_true")
     args = argparser.parse_args()
 
-    only = args.only.split(",") if args.only is not None else None
-    exclude = args.exclude.split(",") if args.exclude is not None else None
+    assert args.path is not None or args.table is not None, "Either --path or --table must be given"
 
-    with TemporaryDirectory() as output_folder:
-        output_folder = Path(output_folder)
-        tables_summary = main(output_folder, only=only, exclude=exclude)
-        with open(args.output, "w") as fd:
-            fd.write(json.dumps(tables_summary, indent=4))
+    # Manage whether to show progress with our handy context manager
+    with display_progress(args.progress):
+
+        # Derive the name of the table from either provided table name or its path
+        table_name = args.table or Path(args.path).stem
+
+        # If we have a path, we can just read the table from there
+        if args.path:
+            table_curr = read_file(args.path)
+
+        # If there is no local path for the table, we have to produce the table ourselves
+        else:
+            with TemporaryDirectory() as output_folder:
+                output_folder = Path(output_folder)
+                pipeline_name = table_name.replace("-", "_")
+                data_pipeline = DataPipeline.load(pipeline_name)
+                (output_folder / "snapshot").mkdir()
+                (output_folder / "intermediate").mkdir()
+                table_curr = data_pipeline.run(pipeline_name, output_folder)
+
+        # Download the table from the production server if it exists
+        try:
+            table_prod = read_file(f"{URL_OUTPUTS_PROD}/{table_name}.csv")
+        except:
+            table_prod = DataFrame(columns=table_curr.columns)
+
+        output_dict = compare_tables(table_curr, table_prod)
+        output_json = json.dumps({"table": table_name, **output_dict}, indent=4)
+        if args.output is None:
+            sys.stdout.write(output_json)
+        else:
+            with open(args.output, "w") as fd:
+                fd.write(output_json)
