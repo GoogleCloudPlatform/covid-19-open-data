ee457aeb0490e8b14890e711ddde0c644afa252e
Mayank Daswani <mdaswani@google.com>
2020-07-09 14:52:18 +0100

    Add Mozambique subregions to epidemiology pipeline. (#252)

---
+++ src/pipelines/epidemiology/mz_humdata.py
@@ -0,0 +1,47 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict
+from pandas import DataFrame
+from lib.data_source import DataSource
+from lib.time import safe_datetime_parse
+
+
+class MozambiqueHumdataDataSource(DataSource):
+    def parse_dataframes(
+        self, dataframes: Dict[str, DataFrame], aux: Dict[str, DataFrame], **parse_opts
+    ) -> DataFrame:
+
+        # Rename the appropriate columns
+        data = (
+            dataframes[0]
+            .rename(
+                columns={
+                    "Date": "date",
+                    "Province": "match_string",
+                    "Positive Cases": "total_confirmed",
+                    "Deaths": "total_deceased",
+                    "Recovered": "total_recovered",
+                }
+            )
+            .drop([0])
+        )
+
+        data.date = data.date.apply(safe_datetime_parse)
+
+        # Make sure all records have the country code
+        data["country_code"] = "MZ"
+
+        # Output the results
+        return data
