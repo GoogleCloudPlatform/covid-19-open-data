a7b76b3478aee39c0f212d2dd149948443374cd2
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2020-07-15 09:57:50 -0400

    Replace ES data source and remove stale PE (#273)

--- src/pipelines/epidemiology/es_authority.py
+++ src/pipelines/epidemiology/es_authority.py
@@ -14,54 +14,50 @@
 
 from typing import Dict
 from pandas import DataFrame
-from lib.io import read_file
 from lib.data_source import DataSource
-from lib.time import datetime_isoformat
+from lib.utils import table_rename, pivot_table
 
 
-class ISCIIIDataSource(DataSource):
-    def parse(self, sources: Dict[str, str], aux: Dict[str, DataFrame], **parse_opts) -> DataFrame:
+class ISCIIIConfirmedDataSource(DataSource):
+    def parse_dataframes(
+        self, dataframes: Dict[str, DataFrame], aux: Dict[str, DataFrame], **parse_opts
+    ) -> DataFrame:
 
-        # Retrieve the CSV files from https://covid19.isciii.es
-        data = (
-            read_file(sources[0], error_bad_lines=False, encoding="ISO-8859-1")
-            .rename(
-                columns={
-                    "FECHA": "date",
-                    "CCAA": "subregion1_code",
-                    "Fallecidos": "total_deceased",
-                    "Hospitalizados": "total_hospitalized",
-                    "UCI": "total_intensive_care",
-                }
-            )
-            .dropna(subset=["date"])
-        )
-
-        # Confirmed cases are split across 2 columns
-        confirmed_columns = ["CASOS", "PCR+"]
-        for col in confirmed_columns:
-            data[col] = data[col].fillna(0)
-        data["total_confirmed"] = data.apply(
-            lambda x: sum([x[col] for col in confirmed_columns]), axis=1
+        confirmed = table_rename(
+            dataframes["confirmed"],
+            {
+                "ccaa_iso": "subregion1_code",
+                "fecha": "date",
+                "num_casos_prueba_pcr": "new_confirmed",
+            },
+            drop=True,
         )
 
         # Convert dates to ISO format
-        data["date"] = data["date"].apply(lambda date: datetime_isoformat(date, "%d/%m/%Y"))
-
-        # Keep only the columns we can process
-        data = data[
-            [
-                "date",
-                "subregion1_code",
-                "total_confirmed",
-                "total_deceased",
-                "total_hospitalized",
-                "total_intensive_care",
-            ]
-        ]
+        confirmed["date"] = confirmed["date"].astype(str)
 
         # Add the country code to all records
-        data["country_code"] = "ES"
+        confirmed["country_code"] = "ES"
 
         # Output the results
-        return data
+        return confirmed
+
+
+class MSCBSDeceasedDataSource(DataSource):
+    def parse_dataframes(
+        self, dataframes: Dict[str, DataFrame], aux: Dict[str, DataFrame], **parse_opts
+    ) -> DataFrame:
+
+        deceased = table_rename(dataframes["deceased"], {"FECHA / CCAA": "date"})
+        deceased = pivot_table(
+            deceased.set_index("date"), value_name="new_deceased", pivot_name="match_string"
+        )
+
+        # Convert dates to ISO format
+        deceased["date"] = deceased["date"].apply(lambda x: str(x)[:10])
+
+        # Add the country code to all records
+        deceased["country_code"] = "ES"
+
+        # Output the results
+        return deceased
