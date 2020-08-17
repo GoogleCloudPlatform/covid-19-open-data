a11e21b9f18b9d111ff64ff7c435fb8898e42782
Mayank Daswani <mdaswani@google.com>
2020-08-17 10:21:38 +0100

    Fix typo in import statement.

--- src/scripts/cloud_error_processing.py
+++ src/scripts/cloud_error_processing.py
@@ -9,7 +9,7 @@ from google.cloud import secretmanager
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from lib.constants import ISSUES_API_URL
-from lib.error_logging import ErrorLogger
+from lib.error_logger import ErrorLogger
 
 
 class GithubIssueHandler(ErrorLogger):
