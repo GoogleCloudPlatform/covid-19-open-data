84cdc7a53719c62c82463e450f0cc555ca7824f2
Oscar Wahltinez <oscar@wahltinez.org>
2020-08-30 15:53:03 -0400

    Handle subregions in Oxford Government Response

--- src/pipelines/oxford_government_response/oxford_government_response.py
+++ src/pipelines/oxford_government_response/oxford_government_response.py
@@ -32,29 +32,50 @@ class OxfordGovernmentResponseDataSource(DataSource):
         # Drop redundant flag columns
         data = data.drop(columns=[col for col in data.columns if "_Flag" in col])
 
-        # Join with ISO data to retrieve our key
+        # Join with ISO data to retrieve our known regions
         data = data.rename(columns={"CountryCode": "3166-1-alpha-3"}).merge(aux["country_codes"])
+        data = data.rename(columns={"3166-1-alpha-2": "country_code"})
+
+        # Make sure subregions are allowed to match this data
+        data = data.drop(columns=["key"])
+        data["subregion1_code"] = None
+        data["subregion2_code"] = None
+        data["locality_code"] = None
+        subregion1_mask = data["RegionCode"].notna()
+        data.loc[subregion1_mask, "subregion1_code"] = data.loc[
+            subregion1_mask, "RegionCode"
+        ].apply(lambda x: x.split("_")[-1])
+
+        # Some region codes are not using the usual ISO standard
+        hotfix_triplets = [("GB", "SCO", "SCT"), ("GB", "WAL", "WLS")]
+        for country_code, subregion1_code, replacement in hotfix_triplets:
+            country_code_mask = data["country_code"] == country_code
+            subregion1_code_mask = data["subregion1_code"] == subregion1_code
+            data.loc[country_code_mask & subregion1_code_mask, "subregion1_code"] = replacement
 
         # Use consistent naming convention for columns
-        data = data[
-            [col for col in data.columns if "_" in col or col in ("date", "key", "StringencyIndex")]
+        keep_columns = [
+            "date",
+            "country_code",
+            "subregion1_code",
+            "subregion2_code",
+            "locality_code",
+            "StringencyIndex",
         ]
-        data.columns = [col.split("_")[-1].lower() for col in data.columns]
+        data = data[[col for col in data.columns if "_" in col or col in keep_columns]]
+        data.columns = [
+            col if col in keep_columns else col.split("_")[-1].lower() for col in data.columns
+        ]
         data.columns = [re.sub(r"\s", "_", col) for col in data.columns]
 
         # Fix column typos
         data = data.rename(
             columns={
-                "stringencyindex": "stringency_index",
+                "StringencyIndex": "stringency_index",
                 "close_public_transport": "public_transport_closing",
                 "debt/contract_relief": "debt_relief",
             }
         )
 
-        # Remove unneeded columns
-        data = data.drop(columns=["wildcard"])
-
-        # Reorder columns and output result
-        first_columns = ["date", "key"]
-        data = data[first_columns + [col for col in data.columns if col not in first_columns]]
-        return data.sort_values(first_columns)
+        # Remove unneeded columns and output
+        return data.drop(columns=["wildcard"])
