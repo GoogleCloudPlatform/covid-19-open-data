cf16fb0e6c63e704de1c4883a28df12f3393625a
Chandan Nath <nathch@google.com>
2021-03-24 12:23:38 +0530

    add lib function add_total_persons_vaccinated_estimate which we can also use in AUS vaccinations

--- src/pipelines/vaccinations/ch_authority.py
+++ src/pipelines/vaccinations/ch_authority.py
@@ -19,6 +19,7 @@ from pandas import DataFrame
 from lib.data_source import DataSource
 from lib.utils import table_merge
 from lib.utils import table_rename
+from lib.vaccinations_utils import add_total_persons_vaccinated_estimate
 
 
 class SwitzerlandDataSource(DataSource):
@@ -68,11 +69,7 @@ class SwitzerlandDataSource(DataSource):
             how="outer",
         )
 
-        # Assuming fully and partially vaccinated persons have 2 and 1 doses respectively,
-        # total_persons_partially_vaccinated = total_vaccine_doses_administered - 2 * total_persons_fully_vaccinated
-        # Therefore, total_persons_vaccinated = total_persons_partially_vaccinated + total_persons_fully_vaccinated
-        # = total_vaccine_doses_administered - total_persons_fully_vaccinated
-        data["total_persons_vaccinated"] = data["total_vaccine_doses_administered"] - data["total_persons_fully_vaccinated"]
+        add_total_persons_vaccinated_estimate(data)
 
         # Make sure all records have the country code and match subregion1 only
         data["key"] = None
