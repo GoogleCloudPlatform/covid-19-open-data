9238b563f943f2879e148b1a015be5baf5a78a04
Jonathan Amar <jonathanamar@verily.com>
2021-05-03 11:56:59 -0400

    added us_cdc_locations for reuse

--- src/pipelines/vaccinations/us_authority_vaccination_type.py
+++ src/pipelines/vaccinations/us_authority_vaccination_type.py
@@ -16,7 +16,7 @@ import json
 from functools import partial
 from lib.concurrent import process_map
 from typing import Dict
-from pandas import DataFrame, concat
+from pandas import DataFrame, concat, read_csv
 from lib.cached_data_source import CachedDataSource
 from lib.utils import table_rename
 from lib.io import read_file
@@ -39,72 +39,13 @@ _column_adapter = {
     "key": "key"
     }
 
-states = [
-    'US',
-    'AK',
-    'AL',
-    'AR',
-    'AS',
-    'AZ',
-    'CA',
-    'CO',
-    'CT',
-    'DC',
-    'DE',
-    'FL',
-    'GA',
-    'GU',
-    'HI',
-    'IA',
-    'ID',
-    'IL',
-    'IN',
-    'KS',
-    'KY',
-    'LA',
-    'MA',
-    'MD',
-    'ME',
-    'MI',
-    'MN',
-    'MO',
-    'MP',
-    'MS',
-    'MT',
-    'NC',
-    'ND',
-    'NE',
-    'NH',
-    'NJ',
-    'NM',
-    'NV',
-    'NY',
-    'OH',
-    'OK',
-    'OR',
-    'PA',
-    'PR',
-    'RI',
-    'SC',
-    'SD',
-    'TN',
-    'TX',
-    'UT',
-    'VA',
-    'VI',
-    'VT',
-    'WA',
-    'WI',
-    'WV',
-    'WY'
- ]
-
+us_states = ['US'] + read_csv("data/us_cdc_locations.csv").key.values.tolist()
 
 def _process_cache_file(file_map: Dict[str, str], date: str) -> DataFrame:
     data = read_file(file_map[date])["vaccination_data"].values.tolist()
     data = DataFrame([list(v.values()) for v in data], columns=list(data[0].keys()))
 
-    data = data.loc[data.Location.isin(states)]
+    data = data.loc[data.Location.isin(us_states)]
     for col in set(_column_adapter.keys()).intersection(data.columns):
         data[col] = data[col].fillna(0).astype(int)
 
