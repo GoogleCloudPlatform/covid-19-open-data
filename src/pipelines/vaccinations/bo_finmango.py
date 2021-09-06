feee05f82a9e5e5075b716f66510d7f1370aab36
Oscar Wahltinez <oscar@wahltinez.org>
2021-09-06 15:33:29 -0400

    Fix BO data source

--- src/pipelines/vaccinations/bo_finmango.py
+++ src/pipelines/vaccinations/bo_finmango.py
@@ -13,11 +13,9 @@
 # limitations under the License.
 
 from typing import Any, Dict
-from pandas import DataFrame
-from lib.cast import safe_int_cast
+from pandas import DataFrame, concat
 from lib.data_source import DataSource
 from lib.utils import table_rename
-from pipelines.epidemiology.de_authority import _SUBREGION1_CODE_MAP
 
 
 _column_adapter = {
@@ -32,7 +30,7 @@ class FinMangoBoliviaDataSource(DataSource):
     def parse_dataframes(
         self, dataframes: Dict[Any, DataFrame], aux: Dict[str, DataFrame], **parse_opts
     ) -> DataFrame:
-        data = table_rename(dataframes[0], _column_adapter, drop=True)
+        data = table_rename(concat(dataframes.values()), _column_adapter, drop=True)
 
         data["country_code"] = "BO"
         data["locality_code"] = None
