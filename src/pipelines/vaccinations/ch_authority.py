2152a9f602f5a7d9066757a7bc9d48839222215b
Oscar Wahltinez <oscar@wahltinez.org>
2021-09-06 16:46:01 -0400

    Fix CH data source

--- src/pipelines/vaccinations/ch_authority.py
+++ src/pipelines/vaccinations/ch_authority.py
@@ -19,24 +19,43 @@ from pandas import DataFrame
 from lib.data_source import DataSource
 from lib.utils import table_merge
 from lib.utils import table_rename
-from lib.vaccinations_utils import estimate_total_persons_vaccinated
 
 
-class SwitzerlandDataSource(DataSource):
+def _output_ch_data(data: DataFrame) -> DataFrame:
+    # Make sure all records have the country code and match subregion1 only
+    data["key"] = None
+    data["country_code"] = "CH"
+    data["subregion2_code"] = None
+    data["locality_code"] = None
+
+    # Country-level records have a known key
+    country_mask = data["subregion1_code"] == "CH"
+    data.loc[country_mask, "key"] = "CH"
+
+    # Principality of Liechtenstein is not in CH but is in the data as FL
+    data.loc[data["subregion1_code"] == "FL", "key"] = "LI"
+
+    # Output the results
+    return data
+
+
+class SwitzerlandPersonsDataSource(DataSource):
     def fetch(
-            self,
-            output_folder: Path,
-            cache: Dict[str, str],
-            fetch_opts: List[Dict[str, Any]],
-            skip_existing: bool = False,
+        self,
+        output_folder: Path,
+        cache: Dict[str, str],
+        fetch_opts: List[Dict[str, Any]],
+        skip_existing: bool = False,
     ) -> Dict[str, str]:
         # the url in the config is a json file which contains the actual dated urls for the data
-        src_url = fetch_opts[0]['url']
+        src_url = fetch_opts[0]["url"]
         data = requests.get(src_url).json()
 
         fetch_opts = [
-            {'name': 'vaccDosesAdministered', 'url': data['sources']['individual']['csv']['vaccDosesAdministered']},
-            {'name': 'fullyVaccPersons', 'url': data['sources']['individual']['csv']['fullyVaccPersons']}
+            {
+                "name": "fullyVaccPersons",
+                "url": data["sources"]["individual"]["csv"]["vaccPersonsV2"],
+            }
         ]
 
         return super().fetch(output_folder, cache, fetch_opts, skip_existing)
@@ -44,46 +63,67 @@ class SwitzerlandDataSource(DataSource):
     def parse_dataframes(
         self, dataframes: Dict[Any, DataFrame], aux: Dict[str, DataFrame], **parse_opts
     ) -> DataFrame:
-        data = table_merge(
-            [
-                table_rename(
-                    dataframes['vaccDosesAdministered'],
-                    {
-                        "date": "date",
-                        "geoRegion": "subregion1_code",
-                        "sumTotal": "total_vaccine_doses_administered",
-                    },
-                    drop=True,
-                ),
-                table_rename(
-                    dataframes['fullyVaccPersons'],
-                    {
-                        "date": "date",
-                        "geoRegion": "subregion1_code",
-                        "sumTotal": "total_persons_fully_vaccinated",
-                    },
-                    drop=True,
-                ),
-            ],
-            on=["date", "subregion1_code"],
-            how="outer",
+        data = table_rename(
+            dataframes["fullyVaccPersons"],
+            {
+                "date": "date",
+                "geoRegion": "subregion1_code",
+                "type": "_statistic",
+                "entries": "_new_count",
+                "sumTotal": "_total_count",
+            },
+            drop=True,
         )
 
-        data["total_persons_vaccinated"] = estimate_total_persons_vaccinated(data)
-
-        # Make sure all records have the country code and match subregion1 only
-        data["key"] = None
-        data["country_code"] = "CH"
-        data["subregion2_code"] = None
-        data["locality_code"] = None
-
-        # Country-level records have a known key
-        country_mask = data["subregion1_code"] == "CH"
-        data.loc[country_mask, "key"] = "CH"
-
-        # Principality of Liechtenstein is not in CH but is in the data as FL
-        country_mask = data["subregion1_code"] == "FL"
-        data.loc[country_mask, "key"] = "LI"
+        # Combine all the different variable indicators
+        tables = []
+        for col, var in {
+            "COVID19AtLeastOneDosePersons": "persons_vaccinated",
+            "COVID19FullyVaccPersons": "persons_fully_vaccinated",
+        }.items():
+            adapter = {"_new_count": f"new_{var}", "_total_count": f"total_{var}"}
+            subset = data.loc[data["_statistic"] == col].drop(columns=["_statistic"])
+            tables.append(subset.rename(columns=adapter))
+        data = table_merge(tables, on=["date", "subregion1_code"], how="outer")
 
         # Output the results
-        return data
+        return _output_ch_data(data)
+
+
+class SwitzerlandDosesDataSource(DataSource):
+    def fetch(
+        self,
+        output_folder: Path,
+        cache: Dict[str, str],
+        fetch_opts: List[Dict[str, Any]],
+        skip_existing: bool = False,
+    ) -> Dict[str, str]:
+        # the url in the config is a json file which contains the actual dated urls for the data
+        src_url = fetch_opts[0]["url"]
+        data = requests.get(src_url).json()
+
+        fetch_opts = [
+            {
+                "name": "vaccDosesAdministered",
+                "url": data["sources"]["individual"]["csv"]["vaccDosesAdministered"],
+            }
+        ]
+
+        return super().fetch(output_folder, cache, fetch_opts, skip_existing)
+
+    def parse_dataframes(
+        self, dataframes: Dict[Any, DataFrame], aux: Dict[str, DataFrame], **parse_opts
+    ) -> DataFrame:
+        data = table_rename(
+            dataframes["vaccDosesAdministered"],
+            {
+                "date": "date",
+                "geoRegion": "subregion1_code",
+                "entries": "new_vaccine_doses_administered",
+                "sumTotal": "total_vaccine_doses_administered",
+            },
+            drop=True,
+        )
+
+        # Output the results
+        return _output_ch_data(data)
