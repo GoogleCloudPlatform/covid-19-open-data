ce5ee6a2845cd271324bf2f99d249830be140265
Oscar Wahltinez <oscar@wahltinez.org>
2021-09-06 20:54:23 -0400

    Fix IN data source

--- src/pipelines/vaccinations/in_covid19india_org.py
+++ src/pipelines/vaccinations/in_covid19india_org.py
@@ -18,8 +18,17 @@ from pandas import DataFrame, melt
 
 from lib.data_source import DataSource
 from lib.time import datetime_isoformat
-from lib.utils import table_merge
+from lib.utils import table_merge, table_rename
 from lib.metadata_utils import country_subregion1s
+
+
+_column_adapter = {
+    "Vaccinated As of": "date",
+    "State": "subregion1_name",
+    "First Dose Administered": "total_persons_vaccinated",
+    "Second Dose Administered": "total_persons_fully_vaccinated",
+    "Total Doses Administered": "total_vaccine_doses_administered",
+}
 
 
 class Covid19IndiaOrgL1DataSource(DataSource):
@@ -27,13 +36,11 @@ class Covid19IndiaOrgL1DataSource(DataSource):
         self, dataframes: Dict[str, DataFrame], aux: Dict[str, DataFrame], **parse_opts
     ) -> DataFrame:
 
-        data = dataframes[0]
-        # Flatten the table
-        data = melt(data, id_vars=["State"], var_name="date", value_name='total_vaccine_doses_administered')
+        data = table_rename(dataframes[0], _column_adapter, drop=True)
         data.date = data.date.apply(lambda x: datetime_isoformat(x, "%d/%m/%Y"))
         # add location keys
         subregion1s = country_subregion1s(aux["metadata"], "IN")
         data = table_merge(
-            [data, subregion1s[['key', 'subregion1_name']]],
-            left_on="State", right_on='subregion1_name', how="inner")
+            [data, subregion1s[["key", "subregion1_name"]]], on=["subregion1_name"], how="inner"
+        )
         return data
